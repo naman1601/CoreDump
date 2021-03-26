8a50b4f8eeb5c8ff687ff80a8b51a579f31a24b8
naman1601 <namanchhaparia1601@gmail.com>
2021-03-26 11:08:17 +0530

    refined messages

--- run.py
+++ run.py
@@ -12,14 +12,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # await message.channel.send(message.author)
+
     if 'nadaan' in message.content and 'baalak' in message.content:
         await message.channel.send('bhosadike!')
 
     if message.content.endswith('nga'):
-    	await message.channel.send(message.content[:-3] + 'nge')
+        text = str(message.author)
+        text = text[:-5]
+        text += ' hindi seekh le chutiye'
+        await message.channel.send(text)
+        await message.channel.send(message.content[:-3] + 'nge')
 
     if(message.content.endswith('karla')):
-    	await message.channel.send(message.content[:-1] + 'e')
+        text = str(message.author)
+        text = text[:-5]
+        text += ' hindi seekh le chutiye'
+        await message.channel.send(text)
+        await message.channel.send(message.content[:-1] + 'e')
 
 tokenfile = open('token.txt', 'r')
 tokenstring = tokenfile.read()
